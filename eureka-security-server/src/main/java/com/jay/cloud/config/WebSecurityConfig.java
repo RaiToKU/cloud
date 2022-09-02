c6453d7b13f5bf3a546eb32494f0cd4b90b87114
raitoku <raito_359279822@qq.com>
2022-09-02 10:18:32 +0800

    init

---
+++ eureka-security-server/src/main/java/com/jay/cloud/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.jay.cloud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author raito
+ * @date 2022/09/01
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
