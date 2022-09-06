8a431ab6987babb99772ba4deeb4380d05cbefcd
raitoku <raito_359279822@qq.com>
2022-09-06 17:28:07 +0800

    init

---
+++ gateway/src/main/java/com/jay/cloud/config/GatewayConfig.java
@@ -0,0 +1,22 @@
+package com.jay.cloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author raito
+ * @date 2022/09/06
+ */
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("path_route2", r -> r.path("/biz/getByUsername")
+                        .uri("http://localhost:8201/biz/getByUsername"))
+                .build();
+    }
+}
